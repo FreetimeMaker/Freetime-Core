f5362e69b775325a0c7933f70111427592fa77be
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-21 12:08:59 +0200

    build(design): drop Material3 dependency

--- Design/build.gradle.kts
+++ Design/build.gradle.kts
@@ -23,7 +23,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     api(libs.androidx.compose.ui)
     api(libs.androidx.compose.foundation)
-    api(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
     api(libs.kyant.backdrop)
     api(libs.kyant.shapes)
