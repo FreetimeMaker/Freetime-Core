f83e217e47a572ab82110ead619737b2da2c069c
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-21 16:10:11 +0200

    build: add Design sample application module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,4 +17,4 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "Freetime Core"
-include(":Core", ":Design", ":Browser", ":Donations")
+include(":Core", ":Design", ":Browser", ":Donations", ":Sample")
