12a5a28948b6b558daf2abf6e2b10ce9f9397d55
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-21 16:14:34 +0200

    build(donations): remove Material3 dependency

--- Donations/build.gradle.kts
+++ Donations/build.gradle.kts
@@ -25,5 +25,4 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.compose.material3)
 }
