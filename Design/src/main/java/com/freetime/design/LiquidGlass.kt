5dad0fee0381929bf5d630be8a1a51330e0ec784
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-25 12:57:22 +0200

    Fix Liquid Glass BoxScope modifier import

--- Design/src/main/java/com/freetime/design/LiquidGlass.kt
+++ Design/src/main/java/com/freetime/design/LiquidGlass.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
